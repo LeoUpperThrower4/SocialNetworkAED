c7a0d8603e3c08994723ddba4f8343273cf93681
Leonardo dos Santos Duarte Silva <ldsds94@gmail.com>
2023-01-26 08:01:13 -0300

    a

--- back-aed/app.py
+++ back-aed/app.py
@@ -3,25 +3,16 @@ from flask_cors import CORS
 from socialNet import SimpleNet
 import json
 
-app = Flask(__name__)
-CORS(app)
-database = SimpleNet()
+# app = Flask(__name__)
+# CORS(app)
+db = SimpleNet()
 
-@app.route('/signup', methods=['POST'])
-def signup():
-    body = json.loads(request.data.decode())
-    name = body['name']
-    email = body['email']
-    password = body['password']
-    hasPrivateName = bool(body['hasPrivateName'])
-    hasPrivateEmail = bool(body['hasPrivateMail'])
-    isCompany = bool(body['isCompany'])
-
+def signup(name, email, password, hasPrivateName, hasPrivateEmail, isCompany):
     tipo = 'person'
     if isCompany:
         tipo = 'company'
 
-    response = database.createAccount(email, password, tipo, name, hasPrivateName, hasPrivateEmail)
+    response = db.createAccount(email, password, tipo, name, hasPrivateName, hasPrivateEmail)
     if response == 'ALREADY_SIGNED_UP':
         return "Usuário já Cadastrado", 401
     else:
@@ -32,7 +23,7 @@ def login():
     body = json.loads(request.data.decode())
     email = body['email']
     password = body['password']
-    user = database.getUser(email)
+    user = db.getUser(email)
     if not user:
         return "Invalid information", 400
     if user.value['password'] != password:
@@ -43,13 +34,13 @@ def login():
 @app.route('/relations', methods=['GET'])
 def load_relations():
     userName = request.args.get('email')
-    user = database.getUser(userName)
-    response = database.getAllConnections(user)
+    user = db.getUser(userName)
+    response = db.getAllConnections(user)
     return jsonify({'relations': response}), 200
 
 @app.route('/entities', methods=['GET'])
 def load_entities():
     search = request.args.get('search')
     searchKey = request.args.get('searchKey')
-    searchResults = database.dumbSearch(searchKey, search)
+    searchResults = db.dumbSearch(searchKey, search)
     return jsonify({'entities': searchResults}), 200
--- back-aed/main.py
+++ back-aed/main.py
@@ -1,13 +1,52 @@
 import matplotlib.pyplot as plt
 from time import sleep
-from app import app, database
+from app import app, db
 
 plt.figure()
 
-try:
-    app.run(host='0.0.0.0', port=3001)
-    while True:
-        sleep(1)
-
-except KeyboardInterrupt:
-    database.dump_to_pkl()
+while True:
+    command = input("Escreava um comando: (1) logar, (2) criar conta, (3) sair: ")
+    if command == "3":
+        database.dump_to_pkl()
+        break
+    elif command == "1":
+        email = input("Email: ")
+        password = input("Senha: ")
+        user = db.getUser(email)
+        if not user:
+            print("Informações inválidas")
+        elif user.value['password'] != password:
+            print("Informações inválidas")
+        else:
+            print("Logado com sucesso")
+            while True:
+                command = input("Escreva um comando: (1) adicionar, (2) remover, (3) encontrar, (4) gerar grafo, (5) sair: ")
+                if command == "5":
+                    database.dump_to_pkl()
+                    break
+                elif command == "1":
+                    email = input("Email: ")
+                    user2 = db.getUser(email)
+                    if not user2:
+                        print("Usuário não encontrado")
+                    else:
+                        db.addConnection(user, user2)
+                        print("Adicionado com sucesso")
+                elif command == "2":
+                    email = input("Email: ")
+                    user2 = db.getUser(email)
+                    if not user2:
+                        print("Usuário não encontrado")
+                    else:
+                        db.removeConnection(user, user2)
+                        print("Removido com sucesso")
+                elif command == "3":
+                    search = input("Pesquisa: ")
+                    searchKey = input("Chave de pesquisa: ")
+                    searchResults = db.dumbSearch(searchKey, search)
+                    print(searchResults)
+                elif command == "4":
+                    db.generateGraph(user)
+                    plt.show()
+                else:
+                    print("Comando inválido")
